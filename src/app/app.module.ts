d25246cb43bdaa4bcb81b0edcb3b770e6b160a94
sonsiho <anson.hs38@gmail.com>
2023-07-12 14:12:11 +0700

    temp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,17 @@ import { IonicStorageModule } from '@ionic/storage-angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Drivers } from '@ionic/storage';
 import * as CordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
@@ -36,9 +47,19 @@ import * as CordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
       driverOrder: [CordovaSQLiteDriver._driver, Drivers.LocalStorage, Drivers.IndexedDB],
       version: 2
     }),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage())
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ScreenTrackingService,UserTrackingService],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'edoc-be1a0',
+    appId: '1:1089694738977:web:d3e4c870e04191dbe95160',
+    storageBucket: 'edoc-be1a0.appspot.com',
+    apiKey: 'AIzaSyD2LRwjsQhz0gggTy1m2D54u_aCh6AotAk',
+    authDomain: 'edoc-be1a0.firebaseapp.com',
+    messagingSenderId: '1089694738977',
+    measurementId: 'G-E2XFVPK31T',
+  },
   production: true,
   apiUrl:'https://edoc.smas.edu.vn/api'
 };
